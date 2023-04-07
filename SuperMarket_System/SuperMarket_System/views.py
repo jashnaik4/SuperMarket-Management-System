4ef7b6b67400d56be58175fa0637525bfa9757fa
aryanbar <aryanbar@gmail.com>
2023-04-07 11:46:19 +0530

    Front page complete, Integration with database complete

---
+++ SuperMarket_System/SuperMarket_System/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.db import connection
+
+from SuperMarket_System.models import Products
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+def showProducts(request):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM products")
+    results = cursor.fetchall()
+    return render(request, 'index.html', {'Products': results})
